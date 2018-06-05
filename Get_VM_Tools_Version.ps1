7b77b7e7383a50baf8135e869242366600d46a7a
Jake Bentz <30503929+jakebentz@users.noreply.github.com>
2018-06-05 10:38:53 -0500

    Update Get_VM_Tools_Version.ps1

--- Get_VM_Tools_Version.ps1
+++ Get_VM_Tools_Version.ps1
@@ -1,3 +1,21 @@
+# /=======================================================================
+# /=
+# /=  Get_VM_Tools_Version.ps1
+# /=
+# /=  AUTHOR: Jake Bentz
+# /=  DATE:	05/25/2018
+# /=
+# /=  REQUIREMENTS: N/A
+# /=
+# /=  DESCRIPTION: This script reads VMware Tools versions for specified guests
+# /=  Twitter: @TripDeezil
+# /=
+# /=  REVISION HISTORY
+# /=   VER  DATE        AUTHOR/EDITOR   COMMENT
+# /=   1.0  05/25/2018	 Jake Bentz	 Created script
+# /=
+# /=======================================================================
+#
 $VMGuest = Read-Host "VM(s)"
 
 New-VIProperty -Name ToolsVersion -ObjectType VirtualMachine -ValueFromExtensionProperty 'Config.tools.ToolsVersion' -Force

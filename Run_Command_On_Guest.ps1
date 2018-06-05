9d85bd3a5503cbda0c4fe1ad31259a5a5514ae1f
Jake Bentz <30503929+jakebentz@users.noreply.github.com>
2018-06-05 10:42:44 -0500

    Update Run_Command_On_Guest.ps1

--- Run_Command_On_Guest.ps1
+++ Run_Command_On_Guest.ps1
@@ -1,3 +1,21 @@
+# /=======================================================================
+# /=
+# /=  Run_Command_On_Guest.ps1.ps1
+# /=
+# /=  AUTHOR: Jake Bentz
+# /=  DATE:	05/25/2018
+# /=
+# /=  REQUIREMENTS: N/A
+# /=
+# /=  DESCRIPTION: This script uses Invoke-VMScript to run a command string on specified guests
+# /=  Twitter: @TripDeezil
+# /=
+# /=  REVISION HISTORY
+# /=   VER  DATE        AUTHOR/EDITOR   COMMENT
+# /=   1.0  05/25/2018	 Jake Bentz	 Created script
+# /=
+# /=======================================================================
+#
 $VM = read-host "VM"
 $command = read-host "Command"
 get-vm $VM | Invoke-VMScript $command

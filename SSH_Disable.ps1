195196cfce79acc88c62b9e6df5cffe6dcd1b208
Jake Bentz <30503929+jakebentz@users.noreply.github.com>
2018-06-05 10:45:06 -0500

    Update SSH_Disable.ps1

--- SSH_Disable.ps1
+++ SSH_Disable.ps1
@@ -1,3 +1,20 @@
+# /=======================================================================
+# /=
+# /=  SSH_Disable.ps1
+# /=
+# /=  AUTHOR: Jake Bentz
+# /=  DATE: 05/25/2018
+# /=
+# /=  REQUIREMENTS: N/A
+# /=
+# /=  DESCRIPTION: This script disables SSH on specified hosts
+# /=  Twitter: @TripDeezil
+# /=
+# /=  REVISION HISTORY
+# /=   VER  DATE        AUTHOR/EDITOR   COMMENT
+# /=   1.0  05/25/2018  Jake Bentz      Created script
+# /=
+# /=======================================================================
+#
 $server = Read-Host "Server Name (wildcard accepted)"
-
 get-vmhost $server |ForEach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where {$_.Key -eq "TSM-SSH"})}

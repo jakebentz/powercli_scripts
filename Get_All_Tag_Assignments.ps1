17519a7d7192ed2d3050a274b5f579942abf070e
Jake Bentz <30503929+jakebentz@users.noreply.github.com>
2018-06-05 10:36:17 -0500

    Update Get_All_Tag_Assignments.ps1

--- Get_All_Tag_Assignments.ps1
+++ Get_All_Tag_Assignments.ps1
@@ -1,1 +1,19 @@
+# /=======================================================================
+# /=
+# /=  Get_All_Tag_Assignments.ps1
+# /=
+# /=  AUTHOR: Jake Bentz
+# /=  DATE:	05/25/2018
+# /=
+# /=  REQUIREMENTS: N/A
+# /=
+# /=  DESCRIPTION: This script gets all tag assignments
+# /=  Twitter: @TripDeezil
+# /=
+# /=  REVISION HISTORY
+# /=   VER  DATE        AUTHOR/EDITOR   COMMENT
+# /=   1.0  05/25/2018	 Jake Bentz	 Created script
+# /=
+# /=======================================================================
+#
 get-vm | Get-TagAssignment | select Entity,Tag | sort-object Entity | export-csv .\All_TagAssignments.csv

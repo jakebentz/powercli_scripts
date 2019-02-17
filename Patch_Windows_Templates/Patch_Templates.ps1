1dd698345653dbb23c3497c33f6fef599324a6bd
Jake Bentz <30503929+jakebentz@users.noreply.github.com>
2019-02-16 21:03:06 -0600

    Add files via upload

--- Patch_Windows_Templates/Patch_Templates.ps1
+++ Patch_Windows_Templates/Patch_Templates.ps1
@@ -30,35 +30,35 @@ If ((Test-Path -Path $Logpath) -ne $true) { New-Item -ItemType directory -Path $
 #
 # Create log file
 $Logfile = ".\output\Patch_Template"+ (get-date -format '_ddMMMyyyy_hhmm') +".txt"
-Write "================================================================" > $Logfile
-Write "Patch Template Output File" >> $Logfile
-Write "================================================================" >> $Logfile
+Write "================================================================" | Tee-Object $Logfile
+Write "Patch Template Output File" | Tee-Object $Logfile -Append
+Write "================================================================" | Tee-Object $Logfile -Append
 #
 # Convert template to VM
-Write "============================" >> $Logfile
+Write "============================" | Tee-Object $Logfile -Append
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Converting to VM "
 $Output = $Output += $Template
-Write $Output >> $Logfile
-Set-Template -Template $Template -ToVM -Confirm:$false >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Set-Template -Template $Template -ToVM -Confirm:$false | Tee-Object $Logfile -Append
 Start-sleep -s 60
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Complete"
-Write $Output >> $Logfile
-Write "============================" >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Write "============================" | Tee-Object $Logfile -Append
 #
 #Start VM
-Write "============================" >> $Logfile
+Write "============================" | Tee-Object $Logfile -Append
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Starting VM "
 $Output = $Output += $Template
-Write $Output >> $Logfile
-Start-VM -VM $Template | Get-VMQuestion | Set-VMQuestion -DefaultOption -Confirm:$false >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Start-VM -VM $Template | Get-VMQuestion | Set-VMQuestion -DefaultOption -Confirm:$false | Tee-Object $Logfile -Append
 Start-sleep -s 120
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Complete"
-Write $Output >> $Logfile
-Write "============================" >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Write "============================" | Tee-Object $Logfile -Append
 #
 #Create variables for Guest OS credentials - This is needed for the Invoke-VMScript cmdlet to be able to execute actions inside the Guest.
 #If you don't want to enter the Guest OS local administrator password as clear text in the script, follow the steps on following link to create a file and store it as an encrypted string: http://stackoverflow.com/questions/6239647/using-powershell-credentials-without-being-prompted-for-a-password
@@ -67,52 +67,52 @@ $OSPwd = cat .\OSPwd.txt | convertto-securestring
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $OSPwd
 #
 #The following is the cmdlet that will invoke the Get-WUInstall inside the GuestVM to install all available Windows updates; optionally results can be exported to a log file to see the patches installed and related results.
-Write "============================" >> $Logfile
+Write "============================" | Tee-Object $Logfile -Append
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Patching VM "
 $Output = $Output += $Template
-Write $Output >> $Logfile
-Invoke-VMScript -ScriptType PowerShell -ScriptText "ipmo PSWindowsUpdate; Get-WUInstall –WindowsUpdate –AcceptAll –AutoReboot" -VM $Template -GuestCredential $Cred >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Invoke-VMScript -ScriptType PowerShell -ScriptText "ipmo PSWindowsUpdate; Get-WUInstall –WindowsUpdate –AcceptAll –AutoReboot" -VM $Template -GuestCredential $Cred | Tee-Object $Logfile -Append
 Start-sleep -s 3600
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Complete"
-Write $Output >> $Logfile
-Write "============================" >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Write "============================" | Tee-Object $Logfile -Append
 #
 #Optionally restart VMGuest one more time in case Windows Update requires it and for whatever reason the –AutoReboot switch didn’t complete it.
-Write "============================" >> $Logfile
+Write "============================" | Tee-Object $Logfile -Append
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Restarting VM "
 $Output = $Output += $Template
-Write $Output >> $Logfile
-Restart-VMGuest -VM $Template -Confirm:$false >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Restart-VMGuest -VM $Template -Confirm:$false | Tee-Object $Logfile -Append
 Start-sleep -s 120
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Complete"
-Write $Output >> $Logfile
-Write "============================" >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Write "============================" | Tee-Object $Logfile -Append
 #
 #After a desired wait period, Shutdown the server
-Write "============================" >> $Logfile
+Write "============================" | Tee-Object $Logfile -Append
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Shutdown VM "
 $Output = $Output += $Template
-Write $Output >> $Logfile
-Shutdown-VMGuest –VM $Template -Confirm:$false >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Shutdown-VMGuest –VM $Template -Confirm:$false | Tee-Object $Logfile -Append
 Start-sleep -s 120
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Complete"
-Write $Output >> $Logfile
-Write "============================" >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Write "============================" | Tee-Object $Logfile -Append
 #
 #Convert VM to Template
-Write "============================" >> $Logfile
+Write "============================" | Tee-Object $Logfile -Append
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Convert VM to Template"
 $Output = $Output += $Template
-Write $Output >> $Logfile
-Set-VM –VM $Template -ToTemplate -Confirm:$false >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Set-VM –VM $Template -ToTemplate -Confirm:$false | Tee-Object $Logfile -Append
 $Output = (get-date -format 'dd-MMM-yyyy hh:mm')
 $Output = $Output += " Complete"
-Write $Output >> $Logfile
-Write "============================" >> $Logfile
+Write $Output | Tee-Object $Logfile -Append
+Write "============================" | Tee-Object $Logfile -Append
